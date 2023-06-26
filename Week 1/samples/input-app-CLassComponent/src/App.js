f8751d40c1d114079e4b7f93f2b9ca12b0f4a48c
Sara Farag <sara.farag10@gmail.com>
2023-06-25 22:00:27 -0700

    Added input-app sample using class component

---
+++ Week 1/samples/input-app-CLassComponent/src/App.js
@@ -0,0 +1,51 @@
+import logo from './logo.svg';
+import './App.css';
+import React from "react";
+
+class App extends React.Component {
+  state = {
+    name: "",
+  };
+
+  // Change the state
+  // Listening to a particular event
+  //wrong not recommonded to change the objet directly we need to do it using function call for asynchronus behaviour
+  // onChangeEventHandler = (eventInfo) => {
+  //   this.setState({ name: eventInfo.currentTarget.value });
+  // };
+
+  //correct way of updateing the states. note state and props paramters in setState here are optional because we don't use them
+  onChangeEventHandler = (e) => {
+    let newName = e.currentTarget.value;
+    this.setState((state, props) => ({ name: newName }));
+  };
+  
+//Mike way
+  // onChangeEventHandler = (eventInfo) => {
+  //   let newName = eventInfo.currentTarget.value;
+
+  //   let fnx = (state, props) => {
+  //     return { name: newName };
+  //   };
+
+  //   this.setState(fnx);
+  // };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Welcome {this.state.name}</p>
+          <input
+            type="text"
+            value={this.state.name}
+            onChange={this.onChangeEventHandler}
+          />
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
