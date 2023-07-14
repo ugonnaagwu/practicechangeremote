70e4b03a7bcde4175fb81da002da9e84b23d64a3
Sara Farag <sara.farag10@gmail.com>
2023-07-14 15:20:37 -0700

    updating Fluent-UI code sample with the new node modules

--- Week 4/Samples/component-library-fluent/src/App.js
+++ Week 4/Samples/component-library-fluent/src/App.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Link, Route } from "react-router-dom";
 import FirstExample from "./FirstExample";
 import SecondExample from "./SecondExample";
 import ThirdExample from "./ThemeExample";
@@ -12,24 +12,20 @@ const App = () => {
           <Link to="/">First Example - DatePicker and Dropdown</Link>
         </li>
         <li>
-          <Link to="/SecondExample">Second Example - Persona With Custom Component and Counter</Link>
+          <Link to="/SecondExample">
+            Second Example - Persona With Custom Component and Counter
+          </Link>
         </li>
         <li>
           <Link to="/ThirdExample">Third (Theme) Example</Link>
         </li>
       </ul>
       <hr />
-      <Switch>
-        <Route exact path="/">
-          <FirstExample />
-        </Route>
-        <Route path="/SecondExample">
-          <SecondExample />
-        </Route>
-        <Route path="/ThirdExample">
-          <ThirdExample />
-        </Route>
-      </Switch>
+      <Routes>
+        <Route path="/" end element={<FirstExample />} />
+        <Route path="/SecondExample" element={<SecondExample />} />
+        <Route path="/ThirdExample" element={<ThirdExample />} />
+      </Routes>
     </Router>
   );
 };
