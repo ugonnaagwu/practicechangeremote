5213a412112d69e80de34509b715991ce103521e
Elisabeth von Spakovsky <elvonspa@microsoft.com>
2024-03-13 10:48:25 -0700

    update bootstrap to functional components

--- Week 3/Samples/react-bootstrap/src/components/BootstrapSelect.js
+++ Week 3/Samples/react-bootstrap/src/components/BootstrapSelect.js
@@ -1,31 +1,27 @@
 import React from "react";
-import { Form, Button, Container } from 'react-bootstrap';
+import { Form, Button, Container } from "react-bootstrap";
 
-class BootstrapSelect extends React.Component {
-
-  render() {
-    return (
-      <div>
-        <h3>Select</h3>
-        <Container>
-          <Form>
-            <Form.Group controlId="selectedCity" >
-              <Form.Label>Select a city</Form.Label>
-              <Form.Control as="select">
-                <option>City 1</option>
-                <option>City 2</option>
-                <option>City 3</option>
-              </Form.Control>
-            </Form.Group>
-            <Button variant="primary" type="submit">
-              Submit
-            </Button>
-          </Form>
-        </Container>
-      </div>
-
-    );
-  }
-}
+const BootstrapSelect = () => {
+  return (
+    <div>
+      <h3>Select</h3>
+      <Container>
+        <Form>
+          <Form.Group controlId="selectedCity">
+            <Form.Label>Select a city</Form.Label>
+            <Form.Control as="select">
+              <option>City 1</option>
+              <option>City 2</option>
+              <option>City 3</option>
+            </Form.Control>
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </Container>
+    </div>
+  );
+};
 
 export default BootstrapSelect;
--- Week 3/Samples/react-bootstrap/src/components/Home.js
+++ Week 3/Samples/react-bootstrap/src/components/Home.js
@@ -1,54 +1,61 @@
-import React from 'react'
+import React from "react";
 
-import { Form } from 'react-bootstrap';
-import { Alert } from 'react-bootstrap';
-import { Image } from 'react-bootstrap';
-import { Button } from 'react-bootstrap';
-import { Container } from 'react-bootstrap';
+import { Form } from "react-bootstrap";
+import { Alert } from "react-bootstrap";
+import { Image } from "react-bootstrap";
+import { Button } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 
 import birthday from "../images/birthday.png";
 
-class Home extends React.Component {
+const Home = () => {
+  // We will use showAlert to determine if we should show the alert
+  const [showAlert, setShowAlert] = React.useState(false);
+  const [dob, setDob] = React.useState(null);
 
-    // We will use showAlert to determine if we should show the alert
-    constructor(props) {
-        super(props);
-        this.state = { showAlert: false, dob: null };
-        this.handleInputChange = this.handleInputChange.bind(this);
-    }
+  const handleInputChange = (e) => {
+    console.log("changeDate");
+    setDob(e.target.value);
+  };
 
-    handleInputChange = (e) => {
-        console.log("changeDate");
-        this.setState({ showAlert: this.state.showAlert, dob: e.target.value })
-    };
+  const saveDate = (e) => {
+    console.log("saveDate");
+    setShowAlert(!showAlert);
+  };
 
-    saveDate = (e) => {
-        console.log("saveDate");
-        this.setState({ showAlert: !this.state.showAlert, dob: this.state.dob });
-    };
-
-    render() {
-        return (
-            <div>
-                <h3>Date / Button / Alert / Rounded Image</h3>
-                <Container>
-                    <Form>
-                        <Form.Group controlId="dob">
-                            <Form.Label>Select your date of birth</Form.Label>
-                            <Form.Control type="date" name="dob" value={this.state.dob || ""} onChange={this.handleInputChange} />
-                        </Form.Group>
-                        <Button variant="primary" onClick={this.saveDate}>Save the date!</Button>
-                        <br></br>
-                        < Alert variant="info" show={this.state.showAlert}>
-                            The date {this.state.dob} is saved!
-                        </Alert >
-                    </Form>
-                    <br></br>
-                    <Image src={birthday} className="border border-primary" width="170" height="170" roundedCircle />
-                </Container>
-            </div>
-        )
-    }
-}
+  return (
+    <div>
+      <h3>Date / Button / Alert / Rounded Image</h3>
+      <Container>
+        <Form>
+          <Form.Group controlId="dob">
+            <Form.Label>Select your date of birth</Form.Label>
+            <Form.Control
+              type="date"
+              name="dob"
+              value={dob || ""}
+              onChange={handleInputChange}
+            />
+          </Form.Group>
+          <Button variant="primary" onClick={saveDate}>
+            Save the date!
+          </Button>
+          <br></br>
+          <Alert variant="info" show={showAlert}>
+            The date {dob} is saved!
+          </Alert>
+        </Form>
+        <br></br>
+        <Image
+          src={birthday}
+          className="border border-primary"
+          width="170"
+          height="170"
+          roundedCircle
+        />
+      </Container>
+    </div>
+  );
+};
 
 export default Home;
